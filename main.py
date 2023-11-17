3b37f119ef016d26cac52fe46ade5216441a2116
user3205 <google@gmail.com>
2023-11-17 17:01:37 +0300

    test

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.Qt import *
+from PyQt5 import uic
+from random import randint
 import sys
+
 
 class NewWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.window()
+        self.do_draw = False
+        uic.loadUi("UI.ui", self)
 
-    def window(self):
-        self.setWindowTitle("Title")
-        self.resize(500, 500)
-        self.move(300, 300)
+        self.button.clicked.connect(self.function)
 
-        self.button = QPushButton("Click on me", self)
-        self.button.resize(150, 50)
-        self.button.move(175, 225)
+    def function(self):
+        self.do_draw = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.do_draw:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_krug(qp)
+            qp.end()
+
+    def draw_krug(self, qp):
+        for i in range(10):
+            radius = randint(20, 100)
+            color = QColor(255, 255, 0)
+            pos = QPoint(randint(1, 500), randint(1, 500))
+
+            qp.setBrush(color)
+            qp.drawEllipse(pos, radius, radius)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
